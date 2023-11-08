72b6aec1817a23b4d60d5d96f712c929e78d7e8d
Zach Gutierrez <zg71603@gmail.com>
2023-11-08 00:13:04 -0800

    Adding variables to JS

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Initialize variables
+var SizeOfPassword;
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerCaseLtr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] ;
+var upperCaseLtr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] ;
+//I used special characters from this IBM website:  https://www.ibm.com/docs/en/zos/2.4.0?topic=commands-alphanumeric-national-special-characters
+var specialChar = ["&", "*", " ", "{", "}", "[", "]", ",", "=", "-", "(", ")", ".", "+", ";", "'","/"] ;
+var acceptUppercase;
+var acceptLowercase;
+var acceptNumericValue;
+var acceptSpecialChar;
+var selected;
